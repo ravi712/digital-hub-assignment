2c0018ab297aabbea328eafd2d7002cb722bc8be
ravi <ravi.g712@gmail.com>
2020-04-19 12:52:21 +0530

    formatting documents

--- backend/route.js
+++ backend/route.js
@@ -1,20 +1,21 @@
-var express = require('express'),
-router = express.Router(),
-fs = require('fs'),
-urlParse = require('url-parse'),
-path = require('path');
+var express = require("express"),
+  router = express.Router(),
+  fs = require("fs"),
+  urlParse = require("url-parse"),
+  path = require("path");
 
-var config = JSON.parse(fs.readFileSync(path.resolve(__dirname,'../config.json'))); // Fetching config
+var config = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, "../config.json"))
+); // Fetching config
 
-config.conn.forEach(function (element){ // creating and exporting routes according to the config.
-    var url = urlParse(element.url);
-    router.get(url.pathname,function (req,res){
-        setTimeout(function() {
-            res.status(200).json( { response: element.timeout});            
-        }, element.timeout);
-        
-    });
-    
+config.conn.forEach(function (element) {
+  // creating and exporting routes according to the config.
+  var url = urlParse(element.url);
+  router.get(url.pathname, function (req, res) {
+    setTimeout(function () {
+      res.status(200).json({ response: element.timeout });
+    }, element.timeout);
+  });
 });
 
 module.exports = router;
